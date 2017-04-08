a5f38d353b53d9ddbbb2d1e9798117d2d9300e6c
Ori Kalinski <ori@mintigo.com>
2017-04-08 03:25:50 +0300

    export the env variable inside the script

--- assignment1/data_filter_launcher.sh
+++ assignment1/data_filter_launcher.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+export DATA_SIZE=$1
+export INPUT_DATA=$2
+export OUTPUT_DATA=$3
 gcc data_filter.c -o ./compiled_filter_data
 ./compiled_filter_data $DATA_SIZE $INPUT_DATA $OUTPUT_DATA
