eccee9cf4441a03670ef3d7fb72dad5f04bea36e
Ori Kalinski <ori@mintigo.com>
2017-07-07 15:21:51 +0300

    some params changes

--- assignment5/message_slot.h
+++ assignment5/message_slot.h
@@ -11,11 +11,11 @@
 /* The major device number. We can't rely on dynamic
  * registration any more, because ioctls need to know
  * it. */
-#define MAJOR_NUM 245
+#define MAJOR_NUM 247
 
 
 /* Set the message of the device driver */
-#define IOCTL_SET_ENC _IOW(MAJOR_NUM, 0, unsigned long)
+#define IOCTL_SET_CHA _IOW(MAJOR_NUM, 0, unsigned long)
 
 #define DEVICE_RANGE_NAME "message_slot"
 #define DEVICE_FILE_NAME "message_slot"
